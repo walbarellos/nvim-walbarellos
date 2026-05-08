f5d5738fdeda50cc41f5185edd6e8ab7f0c36ed3
Willian Albarello <willianalbarellos@gmail.com>
2026-05-08 00:57:24 -0500

    fix(mason): remove cppcheck from ensure_installed (must be system installed)

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -121,7 +121,6 @@ return {
                 "clang-format",-- C/C++
                 "shfmt",       -- Shell
                 -- Linters
-                "cppcheck",    -- C/C++
                 "shellcheck",  -- Shell
                 "eslint_d",    -- JS/TS (versão daemon, muito rápida)
             },
